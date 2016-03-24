984bbf032d62824ec184506bd02f59f4b07bc45d
istatkus <imestat@aol.com>
2016-03-23 21:13:19 -0400

    Added files via upload

--- TeamTestData1.R
+++ TeamTestData1.R
@@ -1,40 +1,61 @@
-## Try some Excel data
+## Try some Excel data from Watson Analytics
+## Set my working directory
 ##
 setwd("C:/Users/irene/Desktop/CourseraDS/TeamTest")
+## make packages available that I might use for the Data Analysis
 install.packages("xlsx")
 library(xlsx)
 install.packages("ggplot2")
 library(ggplot2)
 require(stats)
 library(rpart)
+## 
+## Downloaded the file from website separate read it with R
 ##
 myfile <-read.csv("WA_Fn-UseC_-Marketing-Customer-Value-Analysis.csv", header=TRUE)
 ##
+## quick check of variables
+##
 str(myfile)
 ##
+## another quick checks shows first few records few last
+##
 head(myfile)
-## Descriptive Statistice
-## State
-
+##
+## Descriptive Statistics. One way to understand new data is to start looking at 
+## some descriptive measures of the data. For continuous things like mean, 
+## interquartile ranges, measures of central tendency
+##
+## For Categorical data we can start with table of values and number of observations
+##
+## State - Categorical
+##
 table(myfile$State)
 plot(myfile$State, myfile$Customer.Lifetime.Value/1000, 
      main="Lifetime Value by State", 
      xlab="State", ylab="LifetimeValue ($1,000s)")
-
-## Lifetime Value
+##
+## Lifetime Value - numeric
+##
 summary(myfile$Customer.Lifetime.Value)
 IQR(myfile$Customer.Lifetime.Value)
 boxplot(myfile$Customer.Lifetime.Value)
+##
 ## Income
-
-hist(myfile$Income/1000)
+##
 summary(myfile$Income)
+hist(myfile$Income/1000)
 boxplot(myfile$Income/1000)
-## Resoponse
+##
+## Response - have not found what this could be Contains Yes, No
+##
 table(myfile$Response)
 ##
+## Coverage
+##
 table(myfile$Coverage)
-## some diff
+## some diff type of coverage seems to have an association
+##
 plot(myfile$Coverage, myfile$Customer.Lifetime.Value/1000, 
      main="Lifetime Value by Coverage", 
      xlab="Coverage", ylab="LifetimeValue ($1,000s)")
@@ -42,48 +63,64 @@ plot(myfile$Coverage, myfile$Customer.Lifetime.Value/1000,
 plot(myfile$Coverage, myfile$Total.Claim.Amount, 
      main="Total Claim Amount", 
      xlab="Coverage", ylab="Total Claim Amount")
+##
 ## Education
+##
 table(myfile$Education)
 plot(myfile$Education, myfile$Customer.Lifetime.Value/1000, 
      main="Lifetime Value by Education", 
      xlab="Education", ylab="LifetimeValue ($1,000s)")
+## lots of outliers similar sized boxes
 ##
 plot(myfile$Education, myfile$Total.Claim.Amount, 
      main="Total Claim Amount", 
      xlab="Education", ylab="Total Claim Amount")
+##
+## Doctor and Master are lowe and fewer outliers
+##
 ## Employment Status
+##
 table(myfile$EmploymentStatus)
 plot(myfile$EmploymentStatus, myfile$Customer.Lifetime.Value/1000, 
      main="Lifetime Value by Emp Status", 
      xlab="EmploymentStatus", ylab="LifetimeValue ($1,000s)")
+## boxes similar but outliers are different
 ##
 ##  >>>>differences
 ##
 plot(myfile$EmploymentStatus, myfile$Total.Claim.Amount, 
      main="Total Claim Amount by Emp Status", 
      xlab="Emp Status", ylab="Total Claim Amount")
+## Emp status to Total Claim shows an association also difference in outliers
+##
 ## scatterplot
 plot(myfile$Total.Claim.Amount/1000, myfile$Customer.Lifetime.Value/1000, 
      xlab="Total Claim 000s", 
      ylab="Lifetime Value 000s", 
      main="Claims V Value", pch=2, cex.main=1.5, frame.plot=FALSE , col="blue")
+## shows some linear but doesnt look strong
+##
 ## Gender
 table(myfile$Gender)
 ##
 plot(myfile$Gender, myfile$Total.Claim.Amount, 
      main="Total Claim Amount by Gender", 
      xlab="Gender", ylab="Total Claim Amount")
+## similar
+##
 ## Location Code
 table(myfile$Location.Code)
 plot(myfile$Location.Code, myfile$Customer.Lifetime.Value/1000, 
      main="Lifetime Value by Location", 
      xlab="Location", ylab="LifetimeValue ($1,000s)")
-##
+## similar
 ## >>>>> Differences
 ##
 plot(myfile$Location.Code, myfile$Total.Claim.Amount, 
      main="Total Claim Amount by Location Code", 
      xlab="LocationCode", ylab="Total Claim Amount")
+## But Location to Total Claim seems to have an association
+##
 ## Marital Status
 table(myfile$Marital.Status)
 plot(myfile$Marital.Status, myfile$Customer.Lifetime.Value/1000, 
@@ -92,18 +129,25 @@ plot(myfile$Marital.Status, myfile$Customer.Lifetime.Value/1000,
 plot(myfile$Marital.Status, myfile$Total.Claim.Amount, 
      main="Total Claim Amount", 
      xlab="MaritalStatus", ylab="Total Claim Amount")
+## single shows some association Divorced fewer outliers
+##
 ## Total Claim Amount
+##
 mean(myfile$Total.Claim.Amount)
 hist(myfile$Total.Claim.Amount)
+sd(myfile$Total.Claim.Amount)
+IQR(myfile$Total.Claim.Amount)
+hist(myfile$Total.Claim.Amount)
+##
 ## Months Since Last Claim
+##
 mean(myfile$Months.Since.Last.Claim)
 hist(myfile$Months.Since.Last.Claim)
 mean(myfile$Number.of.Policies)
 hist(myfile$Number.of.Policies)
-
-sd(myfile$Total.Claim.Amount)
-IQR(myfile$Total.Claim.Amount)
-hist(myfile$Total.Claim.Amount)
+##
+## Customer Lifetime Value
+## expected more here, but not found
 ## to Income
 plot(myfile$Income/1000, myfile$Customer.Lifetime.Value/1000, 
      xlab="Income 000s", 
